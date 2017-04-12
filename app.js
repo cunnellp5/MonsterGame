330b255f154d566c310d63a92b99c4c250c0fd02
cunnellp5 <philip.cunnell@colorado.edu>
2017-04-12 16:33:38 -0600

    complete attack log

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ new Vue({
       this.gameIsRunning = true;
       this.playerHealth = 100;
       this.monsterHealth = 100;
+      this.turns = [];
     },
     attack: function() {
         var damage = this.calculateDamage(3, 10)
@@ -22,11 +23,15 @@ new Vue({
         if (this.checkWin()) {
             return;
         }
-
         this.monsterAttacks();
     },
     specialAttack: function() {
-        this.monsterHealth -= this.calculateDamage(10, 20);
+        var damage = this.calculateDamage(10, 20);
+        this.monsterHealth -= damage;
+        this.turns.unshift({
+          isPlayer: true,
+          text: 'Player hits Monster hard for ' + damage
+        });
         if (this.checkWin()) {
             return;
         }
@@ -38,6 +43,10 @@ new Vue({
         } else {
           this.playerHealth = 100;
         }
+        this.turns.unshift({
+          isPlayer: true,
+          text: 'Player heals for 10'
+        });
         this.monsterAttacks();
     },
     giveUp: function() {
